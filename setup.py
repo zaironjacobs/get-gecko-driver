7db06ad7dbe2701de8f4f31b03981ad842b1aab9
Zairon Jacobs <zaijacobs@gmail.com>
2020-12-22 16:48:41 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 name = 'get-gecko-driver'
-version = '0.1.8'
+version = '0.1.9'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
