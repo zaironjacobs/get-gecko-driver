122f0ff52630ba17a9d39769177e8782175dc3aa
Zairon Jacobs <zaijacobs@gmail.com>
2021-02-15 10:14:11 +0100

    retriever refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 name = 'get-gecko-driver'
-version = '0.1.15'
+version = '0.1.16'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
